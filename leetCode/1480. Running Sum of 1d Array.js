11f1799a20143ce557d4149e7f446af7a4f2b7a2
shamimshakir <33302420+shamimshakir@users.noreply.github.com>
2020-06-23 13:37:31 +0600

    Create 1480. Running Sum of 1d Array.js

---
+++ leetCode/1480. Running Sum of 1d Array.js
@@ -0,0 +1,8 @@
+var runningSum = function(nums) {
+    let output = [nums[0]]
+    
+    for(let i = 1; i < nums.length; i++){
+        output.push(output[i - 1] + nums[i])
+    }
+    return output
+};
