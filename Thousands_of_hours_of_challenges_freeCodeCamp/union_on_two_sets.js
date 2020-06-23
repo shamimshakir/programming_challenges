976f4b114a40add38d76026f56ec9a38b04f5799
shamimshakir <33302420+shamimshakir@users.noreply.github.com>
2020-06-23 13:30:19 +0600

    Create union_on_two_sets.js

---
+++ Thousands_of_hours_of_challenges_freeCodeCamp/union_on_two_sets.js
@@ -0,0 +1,51 @@
+class Set {
+  constructor() {
+    // This will hold the set
+    this.dictionary = {};
+    this.length = 0;
+  }
+  // this method will check for the presence of an element and return true or false
+  has(element) {
+    return this.dictionary[element] !== undefined;
+  }
+  // this method will return all the values in the set
+  values() {
+    return Object.keys(this.dictionary);
+  }
+  // this method will add an element to the set
+  add(element) {
+    if (!this.has(element)) {
+      this.dictionary[element] = true;
+      this.length++;
+      return true;
+    }
+
+    return false;
+  }
+  // this method will remove an element from a set
+  remove(element) {
+    if (this.has(element)) {
+      delete this.dictionary[element];
+      this.length--;
+      return true;
+    }
+
+    return false;
+  }
+  // this method will return the size of the set
+  size() {
+    return this.length;
+  }
+  // change code below this line
+  union(setB){
+    let setA = new Set();
+    this.values().forEach(value => {
+      setA.add(value)
+    })
+    setB.values().forEach(value => {
+      setA.add(value)
+    })
+    return setA
+  }
+  // change code above this line
+}
