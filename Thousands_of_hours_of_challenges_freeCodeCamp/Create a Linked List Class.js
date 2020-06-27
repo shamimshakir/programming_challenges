0ba412fd3e0bda056a4722b11188e41d7993f9b7
shamimshakir <33302420+shamimshakir@users.noreply.github.com>
2020-06-27 09:45:49 +0600

    Update Create a Linked List Class.js

--- Thousands_of_hours_of_challenges_freeCodeCamp/Create a Linked List Class.js
+++ Thousands_of_hours_of_challenges_freeCodeCamp/Create a Linked List Class.js
@@ -47,58 +47,6 @@ function LinkedList() {
       }
     }
   };
-
-  function LinkedList() {
-  var length = 0;
-  var head = null;
-
-  var Node = function(element){ // {1}
-    this.element = element;
-    this.next = null;
-  };
-
-  this.size = function() {
-    return length;
-  };
-
-  this.head = function(){
-    return head;
-  };
-
-  this.add = function(element){
-    var node = new Node(element);
-    if(head === null){
-        head = node;
-    } else {
-        var currentNode = head;
-
-        while(currentNode.next){
-            currentNode  = currentNode.next;
-        }
-
-        currentNode.next = node;
-    }
-
-    length++;
-  };
-
-  this.remove = function(element){
-    var currentNode = head;
-    var previousNode;
-    if(currentNode.element === element){
-        head = currentNode.next;
-    } else {
-        while(currentNode.element !== element) {
-            previousNode = currentNode;
-            currentNode = currentNode.next;
-        }
-
-        previousNode.next = currentNode.next;
-    }
-
-    length --;
-  };
-
   // Only change code below this line
   this.isEmpty = function(){
     return this.size() === 0
@@ -127,10 +75,6 @@ function LinkedList() {
     }
     return undefined
   }
-  // Only change code above this line
-}
-
-
   
 }
 
