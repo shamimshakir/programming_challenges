25fe649280fe47dab5c3c45e49f01bb845300464
shamimshakir <33302420+shamimshakir@users.noreply.github.com>
2020-06-27 09:26:59 +0600

    Search within a Linked List

--- Thousands_of_hours_of_challenges_freeCodeCamp/Create a Linked List Class.js
+++ Thousands_of_hours_of_challenges_freeCodeCamp/Create a Linked List Class.js
@@ -47,6 +47,90 @@ function LinkedList() {
       }
     }
   };
+
+  function LinkedList() {
+  var length = 0;
+  var head = null;
+
+  var Node = function(element){ // {1}
+    this.element = element;
+    this.next = null;
+  };
+
+  this.size = function() {
+    return length;
+  };
+
+  this.head = function(){
+    return head;
+  };
+
+  this.add = function(element){
+    var node = new Node(element);
+    if(head === null){
+        head = node;
+    } else {
+        var currentNode = head;
+
+        while(currentNode.next){
+            currentNode  = currentNode.next;
+        }
+
+        currentNode.next = node;
+    }
+
+    length++;
+  };
+
+  this.remove = function(element){
+    var currentNode = head;
+    var previousNode;
+    if(currentNode.element === element){
+        head = currentNode.next;
+    } else {
+        while(currentNode.element !== element) {
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+
+        previousNode.next = currentNode.next;
+    }
+
+    length --;
+  };
+
+  // Only change code below this line
+  this.isEmpty = function(){
+    return this.size() === 0
+  }
+  this.indexOf = function(element){
+    let temp = head;
+    let index = 0;
+    while(temp.next !== null){
+      if(temp.element === element){
+        return index;
+      }
+      temp = temp.next;
+      index++;
+    }
+    return -1
+  }
+  this.elementAt = function(index){
+    let count = 0;
+    let current = head;
+    while(current.next !== null){
+      if(count === index){
+        return current.element;
+      }
+      current = current.next;
+      count++;
+    }
+    return undefined
+  }
+  // Only change code above this line
+}
+
+
   
 }
 
