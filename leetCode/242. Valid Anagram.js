c6a16db96f0c675ad9e7d840174c91f8aca389c0
shamimshakir <33302420+shamimshakir@users.noreply.github.com>
2020-06-29 01:01:55 +0600

    second solution

--- leetCode/242. Valid Anagram.js
+++ leetCode/242. Valid Anagram.js
@@ -6,3 +6,28 @@
 var isAnagram = function(s, t) {
     return s.split('').sort().join('') === t.split('').sort().join('')
 };
+
+
+
+
+// second solution with character map
+var isAnagram = function(s, t) {
+    if(s.length !== t.length)return false;
+    let charMap = {}
+    for(let char of s){
+        if(charMap[char]){
+            charMap[char]++
+        }else{
+            charMap[char] = 1
+        }
+    }
+
+    for(let tc of t){
+      if(!charMap[tc]){
+        return false;
+      }else{
+        charMap[tc] -= 1;
+      }
+    }
+    return true;
+};
