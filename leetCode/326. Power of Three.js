b61e59ee963a04d870e462b0089e83138174ac7e
shamimshakir <33302420+shamimshakir@users.noreply.github.com>
2020-06-30 12:10:59 +0600

    Update 326. Power of Three.js

--- leetCode/326. Power of Three.js
+++ leetCode/326. Power of Three.js
@@ -2,6 +2,10 @@
  * @param {number} n
  * @return {boolean}
  */
+
+// Iterative way, easy to understand
+// Runtime : 288 ms	
+// Memory : 46 MB	
 var isPowerOfThree = function(n) {
     if(n === 1)return true;
     let power3 = 3;
@@ -10,4 +14,15 @@ var isPowerOfThree = function(n) {
       if(power3 === n)return true;
       power3 = power3 * 3;
     }
+};
+
+
+/** Recusion is fun *****/
+// Runtime : 952 ms	
+// Memory : 61.8 MB	
+var isPowerOfThree = function(n) {
+  console.log(n)
+    if(n === 1)return true;
+    if(n < 1)return false;
+    return isPowerOfThree(n / 3)
 };
