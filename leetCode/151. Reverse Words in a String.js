1da316462762c7cf22199d8622791184848decbe
shamimshakir <33302420+shamimshakir@users.noreply.github.com>
2020-06-30 15:04:34 +0600

    Update 151. Reverse Words in a String.js

--- leetCode/151. Reverse Words in a String.js
+++ leetCode/151. Reverse Words in a String.js
@@ -2,6 +2,9 @@
  * @param {string} s
  * @return {string}
  */
+
+// Runtime: 68 ms	
+// Memory: 34.2 MB	
 var reverseWords = function(s) {
  return  s.split(' ').reverse().join(' ').trim().replace(/\s{2,}/g, ' ')
 };
