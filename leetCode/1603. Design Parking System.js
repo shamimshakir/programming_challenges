5a931a51e9509e5b05c31d00d42ac503dae69e19
shamimshakir <33302420+shamimshakir@users.noreply.github.com>
2020-12-20 00:30:50 +0600

    Create 1603. Design Parking System.js

---
+++ leetCode/1603. Design Parking System.js
@@ -0,0 +1,41 @@
+/***
+Runtime: 144 ms, faster than 89.70% of JavaScript online submissions for Design Parking System.
+Memory Usage: 46.1 MB, less than 46.22% of JavaScript online submissions for Design Parking System.
+***/
+
+
+
+/**
+ * @param {number} big
+ * @param {number} medium
+ * @param {number} small
+ */
+var ParkingSystem = function(big, medium, small) {
+    this.big = big;
+    this.medium = medium;
+    this.small = small;
+};
+
+/** 
+ * @param {number} carType
+ * @return {boolean}
+ */
+ParkingSystem.prototype.addCar = function(carType) {
+    if(carType === 1 && this.big > 0){
+        this.big--;
+        return true;
+    }else if(carType === 2 && this.medium > 0){
+        this.medium--;
+        return true;
+    }else if(carType === 3 && this.small > 0){
+        this.small--;
+        return true;
+    }
+    return false;
+};
+
+/** 
+ * Your ParkingSystem object will be instantiated and called as such:
+ * var obj = new ParkingSystem(big, medium, small)
+ * var param_1 = obj.addCar(carType)
+ */
